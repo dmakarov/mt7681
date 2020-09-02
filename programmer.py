09c7c17298f6fe12e5c6ed943fc8613d956079ad
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2020-09-02 20:23:45 +0200

    Cleanup and display progress on verbose option

--- programmer.py
+++ programmer.py
@@ -9,113 +9,18 @@ import socket
 import struct
 import time
 
-# **** ATMEL AVR - A P P L I C A T I O N   N O T E  ************************
-# *
-# * Title:              AVR061 - STK500 Communication Protocol
-# * Filename:           command.h
-# * Version:            1.0
-# * Last updated:       09.09.2002
-# *
-# * Support E-mail:     avr@atmel.com
-# *
-# **************************************************************************
-
-#  *****************[ STK Message constants ]***************************
-
-STK_SIGN_ON_MESSAGE = b'AVR STK'   # Sign on string for Cmnd_STK_GET_SIGN_ON
-
-#  *****************[ STK Response constants ]***************************
-
-Resp_STK_OK = b'\x10'
-Resp_STK_FAILED = b'\x11'
-Resp_STK_UNKNOWN = b'\x12'
-Resp_STK_NODEVICE = b'\x13'
-Resp_STK_INSYNC = b'\x14'
-Resp_STK_NOSYNC = b'\x15'
-
-Resp_ADC_CHANNEL_ERROR = b'\x16'
-Resp_ADC_MEASURE_OK = b'\x17'
-Resp_PWM_CHANNEL_ERROR = b'\x18'
-Resp_PWM_ADJUST_OK = b'\x19'
-
-# *****************[ STK Special constants ]***************************
-
-Sync_CRC_EOP = b'\x20'  # 'SPACE'
-
-# *****************[ STK Command constants ]***************************
-
-Cmnd_STK_GET_SYNC = b'\x30'
-Cmnd_STK_GET_SIGN_ON = b'\x31'
-Cmnd_STK_RESET = b'\x32'
-Cmnd_STK_SINGLE_CLOCK = b'\x33'
-Cmnd_STK_STORE_PARAMETERS = b'\x34'
-
-Cmnd_STK_SET_PARAMETER = b'\x40'
 Cmnd_STK_GET_PARAMETER = b'\x41'
-Cmnd_STK_SET_DEVICE = b'\x42'
-Cmnd_STK_GET_DEVICE = b'\x43'
-Cmnd_STK_GET_STATUS = b'\x44'
-Cmnd_STK_SET_DEVICE_EXT = b'\x45'
-
-Cmnd_STK_ENTER_PROGMODE = b'\x50'
+Cmnd_STK_GET_SYNC = b'\x30'
 Cmnd_STK_LEAVE_PROGMODE = b'\x51'
-Cmnd_STK_CHIP_ERASE = b'\x52'
-Cmnd_STK_CHECK_AUTOINC = b'\x53'
-Cmnd_STK_CHECK_DEVICE = b'\x54'
 Cmnd_STK_LOAD_ADDRESS = b'\x55'
-Cmnd_STK_UNIVERSAL = b'\x56'
-
-Cmnd_STK_PROG_FLASH = b'\x60'
-Cmnd_STK_PROG_DATA = b'\x61'
-Cmnd_STK_PROG_FUSE = b'\x62'
-Cmnd_STK_PROG_LOCK = b'\x63'
 Cmnd_STK_PROG_PAGE = b'\x64'
-Cmnd_STK_PROG_FUSE_EXT = b'\x65'
-
-Cmnd_STK_READ_FLASH = b'\x70'
-Cmnd_STK_READ_DATA = b'\x71'
-Cmnd_STK_READ_FUSE = b'\x72'
-Cmnd_STK_READ_LOCK = b'\x73'
 Cmnd_STK_READ_PAGE = b'\x74'
 Cmnd_STK_READ_SIGN = b'\x75'
-Cmnd_STK_READ_OSCCAL = b'\x76'
-Cmnd_STK_READ_FUSE_EXT = b'\x77'
-Cmnd_STK_READ_OSCCAL_EXT = b'\x78'
-
-# *****************[ STK Parameter constants ]***************************
-
-Parm_STK_HW_VER = b'\x80'  # - R
-Parm_STK_SW_MAJOR = b'\x81'  # - R
-Parm_STK_SW_MINOR = b'\x82'  # - R
-Parm_STK_LEDS = b'\x83'  # - R/W
-Parm_STK_VTARGET = b'\x84'  # - R/W
-Parm_STK_VADJUST = b'\x85'  # - R/W
-Parm_STK_OSC_PSCALE = b'\x86'  # - R/W
-Parm_STK_OSC_CMATCH = b'\x87'  # - R/W
-Parm_STK_RESET_DURATION = b'\x88'  # - R/W
-Parm_STK_SCK_DURATION = b'\x89'  # - R/W
-
-Parm_STK_BUFSIZEL = b'\x90'  # - R/W, Range {0..255}
-Parm_STK_BUFSIZEH = b'\x91'  # - R/W, Range {0..255}
-Parm_STK_DEVICE = b'\x92'  # - R/W, Range {0..255}
-Parm_STK_PROGMODE = b'\x93'  # - 'P' or 'S'
-Parm_STK_PARAMODE = b'\x94'  # - TRUE or FALSE
-Parm_STK_POLLING = b'\x95'  # - TRUE or FALSE
-Parm_STK_SELFTIMED = b'\x96'  # - TRUE or FALSE
-Parm_STK_TOPCARD_DETECT = b'\x98' # - 1 STK502, 2 STK501, 3 no top-card
-
-# *****************[ STK status bit definitions ]***************************
-
-Stat_STK_INSYNC = b'\x01'  # INSYNC status bit, '1' - INSYNC
-Stat_STK_PROGMODE = b'\x02'  # Programming mode,  '1' - PROGMODE
-Stat_STK_STANDALONE = b'\x04'  # Standalone mode,   '1' - SM mode
-Stat_STK_RESET = b'\x08'  # RESET button,      '1' - Pushed
-Stat_STK_PROGRAM = b'\x10'  # Program button, '   1' - Pushed
-Stat_STK_LEDG = b'\x20'  # Green LED status,  '1' - Lit
-Stat_STK_LEDR = b'\x40'  # Red LED status,    '1' - Lit
-Stat_STK_LEDBLINK = b'\x80'  # LED blink ON/OFF,  '1' - Blink
-
-# *****************************[ End Of COMMAND.H ]**************************
+Parm_STK_SW_MAJOR = b'\x81'
+Parm_STK_SW_MINOR = b'\x82'
+Resp_STK_INSYNC = b'\x14'
+Resp_STK_OK = b'\x10'
+Sync_CRC_EOP = b'\x20'
 
 
 class SerialLine:
@@ -177,16 +82,7 @@ class SerialLine:
         return False
 
     def get_params(self):
-        for v in [Parm_STK_HW_VER,
-                  #Parm_STK_TOPCARD_DETECT,
-                  #Parm_STK_VTARGET,
-                  #Parm_STK_VADJUST,
-                  #Parm_STK_OSC_PSCALE,
-                  #Parm_STK_OSC_CMATCH,
-                  #Parm_STK_SCK_DURATION,
-                  #Parm_STK_RESET_DURATION,
-                  Parm_STK_SW_MAJOR,
-                  Parm_STK_SW_MINOR]:
+        for v in [Parm_STK_SW_MAJOR, Parm_STK_SW_MINOR]:
             packet = b''.join([Cmnd_STK_GET_PARAMETER, v, Sync_CRC_EOP])
             reply = b''
             self.sock.sendall(packet)
@@ -207,52 +103,6 @@ class SerialLine:
             return False
         return True
 
-    def set_device(self, devicecode):
-        revision = b'\x00'
-        progtype = b'\x00'
-        parmode = b'\x01'
-        polling = b'\x01'
-        selftimed = b'\x01'
-        pagesizehigh = bytes([0xff & (self.conf['page_size'] >> 8)])
-        pagesizelow = bytes([0xff & self.conf['page_size']])
-        packet = b''.join([Cmnd_STK_SET_DEVICE,
-                           devicecode,
-                           revision,
-                           progtype,
-                           parmode,
-                           polling,
-                           selftimed,
-                           b'\x01',
-                           b'\x03',
-                           b'\xff',
-                           b'\xff',
-                           b'\xff',
-                           b'\xff',
-                           pagesizehigh,
-                           pagesizelow,
-                           b'\x04',
-                           b'\x00',
-                           b'\x00',
-                           b'\x00',
-                           b'\x80',
-                           b'\x00',
-                           Sync_CRC_EOP])
-        self.sock.sendall(packet)
-        print('Sent packet', packet.hex())
-        return self.ack('setting the device')
-
-    def set_device_extended(self):
-        packet = b''.join([Cmnd_STK_SET_DEVICE_EXT,
-                           b'\x04', # this should be \x04, but avrdude uses \x05, bootloader bug?
-                           b'\x04',
-                           b'\xd7',
-                           b'\xc2',
-                           b'\x00',
-                           Sync_CRC_EOP])
-        self.sock.sendall(packet)
-        print('Sent packet', packet.hex())
-        return self.ack('setting the extended device')
-
     def get_signature(self):
         packet = b''.join([Cmnd_STK_READ_SIGN, Sync_CRC_EOP])
         reply = b''
@@ -272,7 +122,6 @@ class SerialLine:
             return None
         print('Timeout reading signature')
         return None
-
     def tx(self, command, action):
         packet = b''.join([command, Sync_CRC_EOP])
         self.sock.sendall(packet)
@@ -292,6 +141,8 @@ class SerialLine:
                 packet = b''.join([Cmnd_STK_LOAD_ADDRESS, ab, Sync_CRC_EOP])
                 self.sock.sendall(packet)
                 if not self.ack('setting address {:x}'.format(addr)):
+                    if self.conf["verbose"] and index > 0:
+                        print()
                     return False
                 page = c.data[index:index + pgsz]
                 if len(page) < pgsz:
@@ -300,9 +151,15 @@ class SerialLine:
                 packet = b''.join([Cmnd_STK_PROG_PAGE, size, b'\x46', page, Sync_CRC_EOP])
                 self.sock.sendall(packet)
                 if not self.ack('programming page'):
+                    if self.conf["verbose"] and index > 0:
+                        print()
                     return False
+                if self.conf["verbose"]:
+                    print('{}..'.format(int(100 * index / c.size)), end=' ', flush=True)
                 addr += int(pgsz / 2)
                 index += pgsz
+        if self.conf["verbose"]:
+            print()
         return True
 
     def verify(self, chunks):
@@ -357,22 +214,14 @@ class SerialLine:
         time.sleep(0.4)
         if not self.sync():
             return
-        # params, set device, enter prog mode are ignored by optiboot bootloader
-        if False:
+        if self.conf['params']:
             if not self.get_params():
                 return
-            if not self.set_device(b'\x86'):
-                return
-            if not self.set_device_extended():
-                return
-            if not self.tx(Cmnd_STK_ENTER_PROGMODE, 'entering program mode'):
-                return
+        if self.conf['signature']:
             sign = self.get_signature()
             if sign is None:
-                self.tx(Cmnd_STK_LEAVE_PROGMODE, 'leaving program mode')
                 return
         if not self.upload(chunks):
-            self.tx(Cmnd_STK_LEAVE_PROGMODE, 'leaving program mode')
             return
         if self.conf['verify']:
             self.verify(chunks)
